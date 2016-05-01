8a74a6c940ceed7c2714f7d36095b00f78ac2eaf
Levi Bostian <levi@curiosityio.com>
2016-04-30 22:00:42 -0400

    Taiga backend not connecting to postgres Dockerfile fixes.

--- taiga-back/checkdb.py
+++ taiga-back/checkdb.py
@@ -1,9 +1,9 @@
 import os, sys, psycopg2
 
-DB_NAME = os.getenv('POSTGRES_DB')
-DB_HOST = os.getenv('POSTGRES_PORT_5432_TCP_ADDR') 
-DB_USER = os.getenv('POSTGRES_USER')
-DB_PASS = os.getenv('POSTGRES_PASSWORD') 
+DB_NAME = os.getenv('TAIGA_DB_NAME')
+DB_HOST = os.getenv('POSTGRES_PORT_5432_TCP_ADDR') or os.getenv('TAIGA_DB_HOST')
+DB_USER = os.getenv('TAIGA_DB_USER')
+DB_PASS = os.getenv('POSTGRES_ENV_POSTGRES_PASSWORD') or os.getenv('TAIGA_DB_PASSWORD')
 
 conn = psycopg2.connect("dbname='" + DB_NAME + "' user='" + DB_USER + "' host='" + DB_HOST + "' password='" + DB_PASS + "'")
 cur = conn.cursor()
